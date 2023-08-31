8b939b3ef43e80894ee8946934bd7fb00c8852ff
Shine <sabirovdima1@gmail.com>
2023-09-01 00:37:19 +0300

    fixes table

--- front-end/src/shared/api/getWalletList.ts
+++ front-end/src/shared/api/getWalletList.ts
@@ -15,28 +15,28 @@ export const getWalletList = async (request: TRequest): Promise<TResponse> => {
           cryptocurrency: { id: 0, name: 'BTC', fullName: 'Bitcoin', algorithmId: 1},
           name: 'Raven',
           source: 'Bybit BYBIT RAVEN',
-          address: '0xfffffaaaaakdshiu4584ASddsa2dab5vd5g425sd4f'
+          address: '0xfffffaaaaakdshiu4584ASddsa2dab5vd5g425sd4fesw'
         },
         {
           id: 1,
           cryptocurrency: { id: 1, name: 'GTC', fullName: 'GreenCoin', algorithmId: 2},
           name: 'HOHOHOHOHOH',
           source: 'Bybit MIble bimble kimple',
-          address: '0xaaaaacccccc425324325324243242678987654678'
+          address: '0xaaaaacccccc425324325324243242678987654678gwe'
         },
         {
           id: 2,
           cryptocurrency: { id: 2, name: 'OXZ', fullName: 'Oxford Crypto', algorithmId: 0},
           name: 'HOHOHOHOHOH',
           source: 'Bybit MIble bimble kimple',
-          address: '0x32424k32425324325324243242678987654678'
+          address: '0x32424k3242532432532424324267898765467851'
         },
         {
           id: 3,
           cryptocurrency: { id: 2, name: 'OXZ', fullName: 'Oxford Crypto', algorithmId: 0},
           name: 'SImple name',
           source: 'Bybit MIble bimble kimple',
-          address: '0x32424k32425324325324243242678987654678'
+          address: '0x32424k32425324325324243242678987654678xc'
         }
       ] })
     }, 1000)
--- front-end/src/widgets/WalletList/model/WalletList.tsx
+++ front-end/src/widgets/WalletList/model/WalletList.tsx
@@ -19,11 +19,13 @@ export const WalletList = (props: WalletListProps) => {
     <FQuadContainer {..._.omit(props, omittedProps)} filled className={(props.className ?? '') + ' ' + styles['wrapper']} bodyProps={{ className: styles['table-wrapper']}}>
       <table>
         <thead>
-          <th>Coin</th>
-          <th>Name</th>
-          <th>Source</th>
-          <th>Address</th>
-          <th></th>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Source</th>
+            <th>Address</th>
+            <th></th> 
+          </tr>
         </thead>
         <tbody>
           {props.list.map(item => <WalletListItem key={item.address} item={item} onUpdate={() => props.onUpdate !== undefined ? props.onUpdate() : null} />)}
