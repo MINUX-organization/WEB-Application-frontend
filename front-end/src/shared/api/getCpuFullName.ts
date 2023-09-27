027f852915b13c500ef3c6f010630a18d9a1b56d
Shine <sabirovdima1@gmail.com>
2023-09-27 08:59:02 +0300

    api fixes

--- front-end/src/shared/api/getCpuFullName.ts
+++ front-end/src/shared/api/getCpuFullName.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import { showNotificationError } from "./lib";
 
 export const getCpuFullName = async () => { 
-    const data: string = await axios.get("http://localhost:8000/static-fata/cpu-full-name")
+    const data: string = await axios.get("http://localhost:8000/static-data/cpu-full-name")
     .then((response) => response.data)
     .catch((error: AxiosError) => {
         showNotificationError(error)
