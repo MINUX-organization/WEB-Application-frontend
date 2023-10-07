29c20877e7ee865c2cb1ce4120fb8b8437f2102c
@i.chuvaev <chuvaevivan1@gmail.com>
2023-10-07 16:16:42 +0700

    fixed last commit

--- front-end/src/entities/gpu/GpuItem.tsx
+++ front-end/src/entities/gpu/GpuItem.tsx
@@ -30,7 +30,7 @@ export default function GpuItem({item, index}: Props) {
         <span className={'span-2'}>{item.fullName}</span> 
         <span className={'span-3'}>{item.shares.accepted}</span> 
         <span className={'span-4'}>{item.shares.rejected}</span> 
-        <span className={'span-5'}>{`${item.hashrate.value?.toFixed(2)} ${item.hashrate.measurement}`}</span> 
+        <span className={'span-5'}>{`${item.hashrate.value?.toFixed(3)} ${item.hashrate.measurement}`}</span> 
         <span className={'span-6'}>{item.temperature + ' °C'}</span>
         <span className={'span-7'}>{item.fanSpeed + ' %'}</span>
         <span className={'span-8'}>{item.powerUsage} <span className="power-unit">W</span></span> 
