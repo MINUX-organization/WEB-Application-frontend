1d12ece968cf25b8f0ce03e957632ca0f28b6576
@i.chuvaev <chuvaevivan1@gmail.com>
2023-10-07 23:47:23 +0700

    fix for FlightSheetListItem

--- front-end/src/entities/FlightSheetListItem/model/FlightSheetListItem.tsx
+++ front-end/src/entities/FlightSheetListItem/model/FlightSheetListItem.tsx
@@ -54,7 +54,7 @@ export const FlightSheetListItem = (props: FlightSheetListItemProps) => {
     updateFlightGPUList: () => {
       isUpdatingGPUList.setTrue();
       editGpusForFlightSheets({
-        gpusForFlightSheets: modifiedGpuList.value.map(v => _.omit(v, 'name', 'conected'))
+        gpusForFlightSheets: modifiedGpuList.value.map(v => _.omit(v, 'name', 'connected'))
       }).then(res => {
         toast.info('updated gpus for flight sheets')
       }).catch(e => {
