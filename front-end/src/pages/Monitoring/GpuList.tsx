3fc9073d412f53d904ed6455d25739ad29f569e0
@i.chuvaev <chuvaevivan1@gmail.com>
2023-10-14 23:00:42 +0700

    splitted crypto and miner in GpuList on monitoring page

--- front-end/src/pages/Monitoring/GpuList.tsx
+++ front-end/src/pages/Monitoring/GpuList.tsx
@@ -1,59 +1,63 @@
 import { useDynamicDataStore } from '@/shared/stores'
-import { CSSProperties, ReactNode, useCallback, useState } from 'react'
+import { CSSProperties, ReactNode, useState } from 'react'
 import { useElementSize } from 'usehooks-ts'
 import { useQuery } from 'react-query'
 import { valueOrZero } from '@/shared/utils'
 import { getCalculationsData } from '@/shared/api/getCalculationsData'
+import { GpuDynamic } from '@/shared/stores/types/GpuDynamic'
 import styles from './Monitoring.module.scss'
 import NaImage from '@/shared/components/NaImage'
+
+const GpuItem = (props: { item: GpuDynamic }) => {
+  const item = props.item;
+  const [isOpen, setIsOpen] = useState(false);
+  const [dropdownRef, size] = useElementSize()
+
+  return (
+    <div className={styles['gpu-item-wrapper']}>
+      <div className={styles['gpu-item']} onClick={() => setIsOpen(prev => !prev)}>
+        <div className={styles['gpu-id-and-fullname']}>
+          <div className={styles['gpu-id']} title={item.id.toString()}>{item.id}</div>
+          <div className={styles['gpu-fullname']}>{item.fullName}</div>
+        </div> 
+        <div>{item.shares.accepted}</div> 
+        <div>{item.shares.rejected}</div> 
+        <div>{item.hashrate.value?.toFixed(3)}<span className={styles['unit']}>{item.hashrate.measurement}</span></div> 
+        <div>{item.temperature}<span className={styles['unit']}>°C</span></div>
+        <div>{item.fanSpeed}<span className={styles['unit']}>%</span></div>
+        <div>{item.powerUsage}<span className={styles['unit']}>W</span></div> 
+      </div>
+      <div className={styles['gpu-dropdown-wrapper'] + ' ' + (isOpen && styles['open'])} style={{ "--gpu-dropdown-width": `${size.width}px`, "--gpu-dropdown-height": `${size.height}px` } as CSSProperties}>
+        <div ref={dropdownRef} className={styles['gpu-dropdown'] + ' ' + (isOpen && styles['open'])}>
+          <div className={styles['gpu-dropdown-first-item']}>
+            <div className={styles['label']}>{"Crypto"}</div>
+            <div className={styles['value']}>{JSON.stringify(item.cryptocurrency)}</div>
+            <div className={styles['label']}>{"Miner"}</div>
+            <div className={styles['value']}>{JSON.stringify(item.miner.fullName)}</div>
+          </div>
+          <div className={styles['gpu-dropdown-second-item']}>
+            <div className={styles['label']}>{"Miner up time"}</div>
+            <div className={styles['value']}>{JSON.stringify(item.minerUpTime)}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const CountBox = (props: { label: string, value: ReactNode, frameColor?: string }) => {
+  return (
+    <div className={styles['count-box']} style={{ "--frame-color": props.frameColor } as CSSProperties}>
+      <div className={styles['label']}>{props.label}</div>
+      <div className={styles['value']}>{props.value}</div>
+    </div>
+  )
+}
 
 export const GpuList = () => {
   const { data } = useQuery(['load calculations data'], getCalculationsData)
   const gpus = [...useDynamicDataStore((state) => state.data.gpus)].sort((a,b) => a.id - b.id)
   const [gpuListRef, size] = useElementSize()
-
-  const CountBox = useCallback((props: { label: string, value: ReactNode, frameColor?: string }) => {
-    return (
-      <div className={styles['count-box']} style={{ "--frame-color": props.frameColor } as CSSProperties}>
-        <div className={styles['label']}>{props.label}</div>
-        <div className={styles['value']}>{props.value}</div>
-      </div>
-    )
-  },[])
-
-  const GpuItem = useCallback((props: { item: typeof gpus[number] }) => {
-    const item = props.item;
-    const [isOpen, setIsOpen] = useState(false);
-    const [dropdownRef, size] = useElementSize()
-    return (
-      <div className={styles['gpu-item-wrapper']}>
-        <div className={styles['gpu-item']} onClick={() => setIsOpen(prev => !prev)}>
-          <div className={styles['gpu-id-and-fullname']}>
-            <div className={styles['gpu-id']} title={item.id.toString()}>{item.id}</div>
-            <div className={styles['gpu-fullname']}>{item.fullName}</div>
-          </div> 
-          <div>{item.shares.accepted}</div> 
-          <div>{item.shares.rejected}</div> 
-          <div>{item.hashrate.value?.toFixed(3)}<span className={styles['unit']}>{item.hashrate.measurement}</span></div> 
-          <div>{item.temperature}<span className={styles['unit']}>°C</span></div>
-          <div>{item.fanSpeed}<span className={styles['unit']}>%</span></div>
-          <div>{item.powerUsage}<span className={styles['unit']}>W</span></div> 
-        </div>
-        <div className={styles['gpu-dropdown-wrapper'] + ' ' + (isOpen && styles['open'])} style={{ "--gpu-dropdown-width": `${size.width}px`, "--gpu-dropdown-height": `${size.height}px` } as CSSProperties}>
-          <div ref={dropdownRef} className={styles['gpu-dropdown'] + ' ' + (isOpen && styles['open'])}>
-            <div className={styles['gpu-dropdown-first-item']}>
-              <div className={styles['label']}>{"Crypto\nminer"}</div>
-              <div className={styles['value']}>{item.cryptocurrency}</div>
-            </div>
-            <div className={styles['gpu-dropdown-second-item']}>
-              <div className={styles['label']}>{"Miner\nup time"}</div>
-              <div className={styles['value']}>{item.minerUpTime}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }, []);
 
   return (
     <div className={styles['gpu-list-wrapper']} style={{ "--gpu-list-width": `${size.width}px`, "--gpu-list-height": `${size.height}px` } as CSSProperties}>
